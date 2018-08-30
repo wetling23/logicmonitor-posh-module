4adb25cfaaf767ef2b79dc5152a561bfc2da9809
Mike Hashemi <mhashemi@synoptek.com>
2018-08-30 12:51:56 -0600

    Fixed a bug getting group ID when a name is provided.

--- Public/Get-LogicMonitorDeviceGroupProperties-Function.ps1
+++ Public/Get-LogicMonitorDeviceGroupProperties-Function.ps1
@@ -1,42 +1,45 @@
 ﻿Function Get-LogicMonitorDeviceGroupProperties {
     <#
-.DESCRIPTION
-    Retrieves all properties (inherited and not) from a selected device group.
-.NOTES 
-    Author: Mike Hashemi
-    V1.0.0.0 date: 2 July 2017
-        - Initial release.
-    V1.0.0.2 date: 23 April 2018
-        - Updated code to allow PowerShell to use TLS 1.1 and 1.2.
-.LINK
-    
-.PARAMETER AccessId
-    Mandatory parameter. Represents the access ID used to connected to LogicMonitor's REST API.    
-.PARAMETER AccessKey
-    Mandatory parameter. Represents the access key used to connected to LogicMonitor's REST API.
-.PARAMETER AccountName
-    Mandatory parameter. Represents the subdomain of the LogicMonitor customer.
-.PARAMETER GroupID
-    Represents ID of the desired device group.
-.PARAMETER GroupName
-    Represents the name of the desired device group. If more than one group has the same name (e.g. "servers"), then they will all be returned.
-.PARAMETER EventLogSource
-    Default value is "LogicMonitorPowershellModule" Represents the name of the desired source, for Event Log logging.
-.PARAMETER BlockLogging
-    When this switch is included, the code will write output only to the host and will not attempt to write to the Event Log.
-.EXAMPLE
-    PS C:\> Get-LogicMonitorDeviceGroups -AccessId <accessId> -AccessKey <accessKey> -AccountName <accountName>
+        .DESCRIPTION
+            Retrieves all properties (inherited and not) from a selected device group.
+        .NOTES 
+            Author: Mike Hashemi
+            V1.0.0.0 date: 2 July 2017
+                - Initial release.
+            V1.0.0.1 date: 23 April 2018
+                - Updated code to allow PowerShell to use TLS 1.1 and 1.2.
+            V1.0.0.2 date: 30 August 2018
+                - Fixed a bug getting group ID when a name is provided.
+                - Updated white space.
+        .LINK
 
-    In this example, the function will search for all device groups and will return their properties.
-.EXAMPLE
-    PS C:\> Get-LogicMonitorDeviceGroups -AccessId <accessId> -AccessKey <accessKey> -AccountName <accountName> -GroupId 6
+        .PARAMETER AccessId
+            Mandatory parameter. Represents the access ID used to connected to LogicMonitor's REST API.
+        .PARAMETER AccessKey
+            Mandatory parameter. Represents the access key used to connected to LogicMonitor's REST API.
+        .PARAMETER AccountName
+            Mandatory parameter. Represents the subdomain of the LogicMonitor customer.
+        .PARAMETER GroupID
+            Represents ID of the desired device group.
+        .PARAMETER GroupName
+            Represents the name of the desired device group. If more than one group has the same name (e.g. "servers"), then they will all be returned.
+        .PARAMETER EventLogSource
+            Default value is "LogicMonitorPowershellModule" Represents the name of the desired source, for Event Log logging.
+        .PARAMETER BlockLogging
+            When this switch is included, the code will write output only to the host and will not attempt to write to the Event Log.
+        .EXAMPLE
+            PS C:\> Get-LogicMonitorDeviceGroups -AccessId <accessId> -AccessKey <accessKey> -AccountName <accountName>
 
-    In this example, the function will search for the device group with "6" in the ID property and will return its properties.
-.EXAMPLE
-    PS C:\> Get-LogicMonitorDeviceGroups -AccessId <accessId> -AccessKey <accessKey> -AccountName <accountName> -GroupName customer1
+            In this example, the function will search for all device groups and will return their properties.
+        .EXAMPLE
+            PS C:\> Get-LogicMonitorDeviceGroups -AccessId <accessId> -AccessKey <accessKey> -AccountName <accountName> -GroupId 6
 
-    In this example, the function will search for the device group with "customer1" in the name property and will return its properties. If more than one group has the same name (e.g. "servers"), then they will all be returned.
-#>
+            In this example, the function will search for the device group with "6" in the ID property and will return its properties.
+        .EXAMPLE
+            PS C:\> Get-LogicMonitorDeviceGroups -AccessId <accessId> -AccessKey <accessKey> -AccountName <accountName> -GroupName customer1
+
+            In this example, the function will search for the device group with "customer1" in the name property and will return its properties. If more than one group has the same name (e.g. "servers"), then they will all be returned.
+    #>
     [CmdletBinding(DefaultParameterSetName = ’AllGroups’)]
     Param (
         [Parameter(Mandatory = $True)]
@@ -61,7 +64,7 @@
 
     If (-NOT($BlockLogging)) {
         $return = Add-EventLogSource -EventLogSource $EventLogSource
-    
+
         If ($return -ne "Success") {
             $message = ("{0}: Unable to add event source ({1}). No logging will be performed." -f (Get-Date -Format s), $EventLogSource)
             Write-Host $message -ForegroundColor Yellow;
@@ -72,7 +75,7 @@
 
     $message = ("{0}: Beginning {1}." -f (Get-Date -Format s), $MyInvocation.MyCommand)
     If ($BlockLogging) {Write-Host $message -ForegroundColor White} Else {Write-Host $message -ForegroundColor White; Write-EventLog -LogName Application -Source $eventLogSource -EntryType Information -Message $message -EventId 5417}
-    
+
     $message = ("{0}: Operating in the {1} parameter set." -f (Get-Date -Format s), $PsCmdlet.ParameterSetName)
     If (($BlockLogging) -AND ($PSBoundParameters['Verbose'])) {Write-Verbose $message} ElseIf ($PSBoundParameters['Verbose']) {Write-Verbose $message; Write-EventLog -LogName Application -Source $eventLogSource -EntryType Information -Message $message -EventId 5417}
 
@@ -94,8 +97,8 @@
     # Update $resourcePath to filter for a specific device.
     Switch ($PsCmdlet.ParameterSetName) {
         "NameFilter" {	
-            $group = Get-LogicMonitorGroups -AccessId $AccessId -AccessKey $AccessKey -AccountName $AccountName -GroupName $GroupName -EventLogSource $EventLogSource
-            
+            $group = Get-LogicMonitorDeviceGroups -AccessId $AccessId -AccessKey $AccessKey -AccountName $AccountName -GroupName $GroupName -EventLogSource $EventLogSource
+
             $groupId = $group.id
         }
     }
@@ -110,7 +113,7 @@
 
     # Get current time in milliseconds
     $epoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
-        
+
     # Concatenate Request Details
     $requestVars = $httpVerb + $epoch + $resourcePath
 
@@ -137,9 +140,9 @@
         $message = ("{0}: It appears that the web request failed. Check your credentials and try again. To prevent errors, {1} will exit. The specific error message is: {2}" `
                 -f (Get-Date -Format s), $MyInvocation.MyCommand, $_.Message.Exception)
         If ($BlockLogging) {Write-Host $message -ForegroundColor White} Else {Write-Host $message -ForegroundColor White; Write-EventLog -LogName Application -Source $eventLogSource -EntryType Information -Message $message -EventId 5417}
-        
+
         Return
     }
 
     Return $response.data.items
-} #1.0.0.1
+} #1.0.0.2
