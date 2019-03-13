d68f3172cc91abbf3cac81959004207055b3f4d6
Mike Hashemi <mhashemi@synoptek.com>
2019-03-13 08:15:16 -0600

    Updated output to only output status on 'verbose'.

--- Public/Add-EventLogSource-Function.ps1
+++ Public/Add-EventLogSource-Function.ps1
@@ -1,29 +1,32 @@
 Function Add-EventLogSource {
     <#
-    .DESCRIPTION
-        Adds an Event Log source, for script/module logging. Adding an Event Log source requires administrative rights.
-    .NOTES 
-        Author: Mike Hashemi
-        V1.0.0.0 date: 19 April 2017
-            - Initial release.
-        V1.0.0.1 date: 1 May 2017
-            - Minor updates to status handling.
-        V1.0.0.2 date: 4 May 2017
-            - Added additional return value.
-        V1.0.0.3 date: 22 May 2017
-            - Changed output to reduce the number of "Write-Host" messages.
-        V1.0.0.4 date: 21 June 2017
-            - Fixed typo.
-            - Significantly improved performance.
-            - Changed logging.
-        V1.0.0.5 date: 21 June 2017
-            - Added a return value if the event log source exists.
-        V1.0.0.6 date: 28 June 2017
-            - Added [CmdletBinding()].
-        V1.0.0.7 date: 28 June 2017
-            - Added a check for the source, then a check on the status of the query.
-    .PARAMETER EventLogSource
-        Mandatory parameter. This parameter is used to specify the event source, that script/modules will use for logging.
+        .DESCRIPTION
+            Adds an Event Log source, for script/module logging. Adding an Event Log source requires administrative rights.
+        .NOTES 
+            Author: Mike Hashemi
+            V1.0.0.0 date: 19 April 2017
+                - Initial release.
+            V1.0.0.1 date: 1 May 2017
+                - Minor updates to status handling.
+            V1.0.0.2 date: 4 May 2017
+                - Added additional return value.
+            V1.0.0.3 date: 22 May 2017
+                - Changed output to reduce the number of "Write-Host" messages.
+            V1.0.0.4 date: 21 June 2017
+                - Fixed typo.
+                - Significantly improved performance.
+                - Changed logging.
+            V1.0.0.5 date: 21 June 2017
+                - Added a return value if the event log source exists.
+            V1.0.0.6 date: 28 June 2017
+                - Added [CmdletBinding()].
+            V1.0.0.7 date: 28 June 2017
+                - Added a check for the source, then a check on the status of the query.
+            V1.0.0.8 date: 13 March 2018
+                - Updated whitespace.
+                - Updated output to only output status on 'verbose'.
+        .PARAMETER EventLogSource
+            Mandatory parameter. This parameter is used to specify the event source, that script/modules will use for logging.
     #>
     [CmdletBinding()]
     Param (
@@ -31,41 +34,41 @@ Function Add-EventLogSource {
         $EventLogSource
     )
         
-        $message = ("{0}: Beginning {1}." -f (Get-Date -Format s), $MyInvocation.MyCommand)
+    $message = ("{0}: Beginning {1}." -f (Get-Date -Format s), $MyInvocation.MyCommand)
+    If ($PSBoundParameters['Verbose']) {Write-Verbose $message}
+
+    # Check if $EventLogSource exists as a source. If the shell is not elevated and the check fails to access the Security log, assume the source does not exist.
+    Try {
+        $sourceExists = [System.Diagnostics.EventLog]::SourceExists("$EventLogSource")
+    }
+    Catch {
+        $sourceExists = $False
+    }
+
+    If ($sourceExists -eq $False) {
+        $message = ("{0}: The event source `"{1}`" does not exist. Prompting for elevation." -f (Get-Date -Format s), $EventLogSource)
         Write-Host $message -ForegroundColor White
-    
-        # Check if $EventLogSource exists as a source. If the shell is not elevated and the check fails to access the Security log, assume the source does not exist.
+
         Try {
-            $sourceExists = [System.Diagnostics.EventLog]::SourceExists("$EventLogSource")
+            Start-Process PowerShell -Verb RunAs -ArgumentList "New-EventLog -LogName Application -Source $EventLogSource -ErrorAction Stop"
+        }
+        Catch [System.InvalidOperationException] {
+            $message = ("{0}: It appears that the user cancelled the operation." -f (Get-Date -Format s))
+            Write-Host $message -ForegroundColor Yellow
+            Return "Error"
         }
         Catch {
-            $sourceExists = $False
+            $message = ("{0}: Unexpected error launching an elevated Powershell session. The specific error is: {1}" -f (Get-Date -Format s), $_.Exception.Message)
+            Write-Host $message -ForegroundColor Red
+            Return "Error"
         }
-    
-        If ($sourceExists -eq $False) {
-            $message = ("{0}: The event source `"{1}`" does not exist. Prompting for elevation." -f (Get-Date -Format s), $EventLogSource)
-            Write-Host $message -ForegroundColor White
-            
-            Try {
-                Start-Process PowerShell -Verb RunAs -ArgumentList "New-EventLog -LogName Application -Source $EventLogSource -ErrorAction Stop"
-            }
-            Catch [System.InvalidOperationException] {
-                $message = ("{0}: It appears that the user cancelled the operation." -f (Get-Date -Format s))
-                Write-Host $message -ForegroundColor Yellow
-                Return "Error"
-            }
-            Catch {
-                $message = ("{0}: Unexpected error launching an elevated Powershell session. The specific error is: {1}" -f (Get-Date -Format s), $_.Exception.Message)
-                Write-Host $message -ForegroundColor Red
-                Return "Error"
-            }
-    
-            Return "Success"
-        }
-        Else {
-            $message = ("{0}: The event source `"{1}`" already exists. There is no action for {2} to take." -f (Get-Date -Format s), $EventLogSource, $MyInvocation.MyCommand)
-            Write-Verbose $message
-    
-            Return "Success"
-        }
-    } #1.0.0.7
+
+        Return "Success"
+    }
+    Else {
+        $message = ("{0}: The event source `"{1}`" already exists. There is no action for {2} to take." -f (Get-Date -Format s), $EventLogSource, $MyInvocation.MyCommand)
+        Write-Verbose $message
+
+        Return "Success"
+    }
+} #1.0.0.8
