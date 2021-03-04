1ed15f89d5ef7b8ba410a77113293c026e205f72
Mike Hashemi <mhashemi@synoptek.com>
2021-03-04 07:52:12 -0700

    added cmdlet

---
+++ Public/Add-LogicMonitorDashboardWidget-Function.ps1
@@ -0,0 +1,162 @@
+Function Add-LogicMonitorDashboardWidget {
+    <#
+        .DESCRIPTION
+            Create a new LogicMonitor dashboard widget.
+        .NOTES
+            Author: Mike Hashemi
+            V1.0.0.0 date: 2 March 2021
+                - Initial release.
+        .LINK
+            https://github.com/wetling23/logicmonitor-posh-module
+        .PARAMETER AccessId
+            Mandatory parameter. Represents the access ID used to connected to LogicMonitor's REST API.
+        .PARAMETER AccessKey
+            Mandatory parameter. Represents the access key used to connected to LogicMonitor's REST API.
+        .PARAMETER AccountName
+            Mandatory parameter. Represents the subdomain of the LogicMonitor customer.
+        .PARAMETER Properties
+            Mandatory parameter. Represents the properties values of the new dashboard. Required fields are "name" and "parentId". Valid properties can be found at https://www.logicmonitor.com/swagger-ui-master/dist/#/Device%20Groups/addDashboard.
+        .PARAMETER BlockStdErr
+            When set to $True, the script will block "Write-Error". Use this parameter when calling from wscript. This is required due to a bug in wscript (https://groups.google.com/forum/#!topic/microsoft.public.scripting.wsh/kIvQsqxSkSk).
+        .PARAMETER EventLogSource
+            When included, (and when LogPath is null), represents the event log source for the Application log. If no event log source or path are provided, output is sent only to the host.
+        .PARAMETER LogPath
+            When included (when EventLogSource is null), represents the file, to which the cmdlet will output will be logged. If no path or event log source are provided, output is sent only to the host.
+        .EXAMPLE
+            PS C:\> $table = @{name = 'widget1'; dashboardId = 1}
+            PS C:\> Add-LogicMonitorDashboard -AccessId <access Id> -AccessKey <access key> -AccountName <account name> -Properties $table
+
+            In this example, the function will create a new dashboard widget with the following properties:
+                - Name: widget1
+                - Dashboard ID: 1
+            Limited logging output is sent to the host.
+        .EXAMPLE
+            PS C:\> $table = @{
+                        name = 'widget1'
+                        dashboardId = 1
+                        type = 'text'
+                        content = 'text content'
+                        description = 'This is a test widget.'
+                        interval = 15
+                        theme = 'borderPurple'
+                    }
+            PS C:\> Add-LogicMonitorDashboard -AccessId <access Id> -AccessKey <access key> -AccountName <account name> -Properties $table -Verbose -LogPath log.txt
+
+            In this example, the function will create a new dashboard with the following properties:
+                - Name: widget1
+                - Dashboard ID: 1
+                - Type: text
+                - Description: This is a test widget.
+                - Widget update interval: 15 minutes
+                - Theme: boarderPurple
+            The widget will be a text widget (with "text content" inside) that updates every 15 minutes. Verbose logging output is sent to the host and log.txt in the current directory.
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory)]
+        [string]$AccessId,
+
+        [Parameter(Mandatory)]
+        [securestring]$AccessKey,
+
+        [Parameter(Mandatory)]
+        [string]$AccountName,
+
+        [Parameter(Mandatory)]
+        [hashtable]$Properties,
+
+        [boolean]$BlockStdErr = $false,
+
+        [string]$EventLogSource,
+
+        [string]$LogPath
+    )
+
+    $message = ("{0}: Beginning {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $MyInvocation.MyCommand)
+    If ($PSBoundParameters['Verbose'] -or $VerbosePreference -eq 'Continue') { If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Verbose -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Verbose -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Verbose -Message $message } }
+
+    # Initialize variables.
+    $httpVerb = "POST" # Define what HTTP operation will the script run.
+    $resourcePath = "/dashboard/widgets"
+    $AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+    # Checking for the required properties
+    If (-NOT($Properties.ContainsKey('name'))) {
+        $message = ("{0}: No widget name provided. Please update the provided properties and re-submit the request.")
+        If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Error -Message $message -BlockStdErr $BlockStdErr } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Error -Message $message -BlockStdErr $BlockStdErr } Else { Out-PsLogging -ScreenOnly -MessageType Error -Message $message -BlockStdErr $BlockStdErr }
+
+        Return "Error"
+    }
+    If (-NOT($Properties.ContainsKey('dashboardId'))) {
+        $message = ("{0}: No dashboard ID provided. Please update the provided properties and re-submit the request.")
+        If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Error -Message $message -BlockStdErr $BlockStdErr } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Error -Message $message -BlockStdErr $BlockStdErr } Else { Out-PsLogging -ScreenOnly -MessageType Error -Message $message -BlockStdErr $BlockStdErr }
+
+        Return "Error"
+    }
+    If (-NOT($Properties.ContainsKey('type'))) {
+        $message = ("{0}: No widget type provided. Please update the provided properties and re-submit the request.")
+        If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Error -Message $message -BlockStdErr $BlockStdErr } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Error -Message $message -BlockStdErr $BlockStdErr } Else { Out-PsLogging -ScreenOnly -MessageType Error -Message $message -BlockStdErr $BlockStdErr }
+
+        Return "Error"
+    }
+
+    $data = ($Properties | ConvertTo-Json -depth 5)
+
+    # Construct the query URL.
+    $url = "https://$AccountName.logicmonitor.com/santaba/rest$resourcePath"
+
+    # Get current time in milliseconds
+    $epoch = [Math]::Round((New-TimeSpan -start (Get-Date -Date "1/1/1970") -end (Get-Date).ToUniversalTime()).TotalMilliseconds)
+
+    # Concatenate Request Details
+    $requestVars = $httpVerb + $epoch + $data + $resourcePath
+
+    # Construct Signature
+    $hmac = New-Object System.Security.Cryptography.HMACSHA256
+    $hmac.Key = [Text.Encoding]::UTF8.GetBytes([System.Runtime.InteropServices.Marshal]::PtrToStringAuto(([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($AccessKey))))
+    $signatureBytes = $hmac.ComputeHash([Text.Encoding]::UTF8.GetBytes($requestVars))
+    $signatureHex = [System.BitConverter]::ToString($signatureBytes) -replace '-'
+    $signature = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($signatureHex.ToLower()))
+
+    # Construct Headers
+    $headers = @{
+        "Authorization" = "LMv1 $accessId`:$signature`:$epoch"
+        "Content-Type"  = "application/json"
+        "X-Version"     = 2
+    }
+
+    # Make Request
+    $message = ("{0}: Executing the REST query ({1})." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $url)
+    If ($PSBoundParameters['Verbose'] -or $VerbosePreference -eq 'Continue') { If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Verbose -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Verbose -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Verbose -Message $message } }
+
+    Try {
+        $response = Invoke-RestMethod -Uri $url -Method $httpVerb -Header $headers -Body $data -ErrorAction Stop
+    }
+    Catch {
+        If ($_.Exception.Message -match '429') {
+            $message = ("{0}: Rate limit exceeded, retrying in 60 seconds." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $MyInvocation.MyCommand, $_.Exception.Message)
+            If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Warning -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Warning -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Warning -Message $message }
+
+            Start-Sleep -Seconds 60
+        }
+        Else {
+            $errormsg = Try{($_ | ConvertFrom-Json -ErrorAction SilentlyContinue | Select-Object -ExpandProperty errorMessage)}Catch{$error[1].Exception.Message}
+            $errorcode = Try{($_ | ConvertFrom-Json -ErrorAction SilentlyContinue | Select-Object -ExpandProperty errorCode)}Catch{"none"}
+            $message = ("{0}: Unexpected error adding DashboardGroup called `"{1}`". To prevent errors, the cmdlet will exit. If present, the following details were returned:`r`n
+                Error message: {2}`r
+                Error code: {3}`r
+                Invoke-Request: {4}`r
+                Headers: {5}`r
+                Body: {6}" -f
+                ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $Properties.Name, $errormsg, $errorcode, $_.Exception.Message, ($headers | Out-String), ($data | Out-String)
+            )
+            If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Error -Message $message -BlockStdErr $BlockStdErr } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Error -Message $message -BlockStdErr $BlockStdErr } Else { Out-PsLogging -ScreenOnly -MessageType Error -Message $message -BlockStdErr $BlockStdErr }
+        }
+
+        Return "Error"
+    }
+
+    $response
+}
+#1.0.0.0
