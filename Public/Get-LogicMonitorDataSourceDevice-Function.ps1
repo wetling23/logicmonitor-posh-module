9f1d4a8222dd51a1a2c92fae6a15b234620a24ef
Mike Hashemi <mhashemi@synoptek.com>
2021-07-06 12:42:32 -0600

    Uploading module version 1.0.1.74. Release notes: Initial release of Get-LogicMonitorDataSourceDevice.

---
+++ Public/Get-LogicMonitorDataSourceDevice-Function.ps1
@@ -0,0 +1,184 @@
+Function Get-LogicMonitorDataSourceDevice {
+    <#
+        .DESCRIPTION
+            Returns a list of LogicMonitor devices with an associated with a specific DataSource.
+        .NOTES
+            Author: Mike Hashemi
+            V1.0.0.0 date: 6 July 2021
+                -Initial release.
+        .LINK
+            https://github.com/wetling23/logicmonitor-posh-module
+        .PARAMETER AccessId
+            Represents the access ID used to connected to LogicMonitor's REST API.
+        .PARAMETER AccessKey
+            Represents the access key used to connected to LogicMonitor's REST API.
+        .PARAMETER AccountName
+            Mandatory parameter. Represents the subdomain of the LogicMonitor customer.
+        .PARAMETER Id
+            Represents the ID of the desired DataSource.
+        .PARAMETER BatchSize
+            Default value is 1000. Represents the number of devices to request from LogicMonitor, in a single batch.
+        .PARAMETER BlockStdErr
+            When set to $True, the script will block "Write-Error". Use this parameter when calling from wscript. This is required due to a bug in wscript (https://groups.google.com/forum/#!topic/microsoft.public.scripting.wsh/kIvQsqxSkSk).
+        .PARAMETER BlockStdErr
+            When set to $True, the script will block "Write-Error". Use this parameter when calling from wscript. This is required due to a bug in wscript (https://groups.google.com/forum/#!topic/microsoft.public.scripting.wsh/kIvQsqxSkSk).
+        .PARAMETER EventLogSource
+            When included, (and when LogPath is null), represents the event log source for the Application log. If no event log source or path are provided, output is sent only to the host.
+        .PARAMETER LogPath
+            When included (when EventLogSource is null), represents the file, to which the cmdlet will output will be logged. If no path or event log source are provided, output is sent only to the host.
+        .EXAMPLE
+            PS C:\> Get-LogicMonitorDataSourceDevice -AccessId <accessId> -AccessKey <accessKey> -AccountName <accountName> -Id 6 -Verbose
+
+            In this example, the cmdlet returns all devices associated with the DataSource with ID 6. Verbose output is sent to the host.
+        .EXAMPLE
+            PS C:\> "6" | Get-LogicMonitorDataSourceDevice -AccessId <accessId> -AccessKey <accessKey> -AccountName <accountName>
+
+            In this example, the cmdlet returns all devices associated with the DataSource with ID 6.
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory)]
+        [string]$AccessId,
+
+        [Parameter(Mandatory)]
+        [securestring]$AccessKey,
+
+        [Parameter(Mandatory)]
+        [string]$AccountName,
+
+        [Parameter(Mandatory, ValueFromPipeline = $true)]
+        [int]$Id,
+
+        [int]$BatchSize = 1000,
+
+        [boolean]$BlockStdErr = $false,
+
+        [string]$EventLogSource,
+
+        [string]$LogPath
+    )
+
+    $message = ("{0}: Beginning {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $MyInvocation.MyCommand)
+    If ($PSBoundParameters['Verbose'] -or $VerbosePreference -eq 'Continue') { If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Verbose -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Verbose -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Verbose -Message $message } }
+
+    # Initialize variables.
+    $devices = [System.Collections.Generic.List[PSObject]]::New() # Primary collection to be filled with Invoke-RestMethod response.
+    [string]$httpVerb = "GET" # Define what HTTP operation will the script run.
+    [string]$resourcePath = "/setting/datasources/$Id/devices" # Define the resourcePath.
+    $queryParams = $null
+    $offset = 0 # Define how many agents from zero, to start the query. Initial is zero, then it gets incremented later.
+    $firstLoopDone = $false # Will change to true, once the function determines how many times it needs to loop, to retrieve all devices.
+    [boolean]$stopLoop = $false # Ensures we run Invoke-RestMethod at least once.
+    $AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+    $message = ("{0}: The resource path is: {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $resourcePath)
+    If ($PSBoundParameters['Verbose'] -or $VerbosePreference -eq 'Continue') { If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Verbose -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Verbose -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Verbose -Message $message } }
+
+    Do {
+        $queryParams = "?offset=$offset&size=$BatchSize&sort=id"
+
+        If ($firstLoopDone -eq $false) {
+            $message = ("{0}: Building request header." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+            If ($PSBoundParameters['Verbose'] -or $VerbosePreference -eq 'Continue') { If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Verbose -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Verbose -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Verbose -Message $message } }
+
+            # Get current time in milliseconds
+            $epoch = [Math]::Round((New-TimeSpan -Start (Get-Date -Date "1/1/1970") -End (Get-Date).ToUniversalTime()).TotalMilliseconds)
+
+            # Concatenate Request Details
+            $requestVars = $httpVerb + $epoch + $resourcePath
+
+            # Construct Signature
+            $hmac = New-Object System.Security.Cryptography.HMACSHA256
+            $hmac.Key = [Text.Encoding]::UTF8.GetBytes([System.Runtime.InteropServices.Marshal]::PtrToStringAuto(([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($AccessKey))))
+            $signatureBytes = $hmac.ComputeHash([Text.Encoding]::UTF8.GetBytes($requestVars))
+            $signatureHex = [System.BitConverter]::ToString($signatureBytes) -replace '-'
+            $signature = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($signatureHex.ToLower()))
+
+            # Construct Headers
+            $headers = @{
+                "Authorization" = "LMv1 $accessId`:$signature`:$epoch"
+                "Content-Type"  = "application/json"
+                "X-Version"     = 2
+            }
+        }
+
+        $url = "https://$AccountName.logicmonitor.com/santaba/rest$resourcePath$queryParams"
+
+        # Make Request
+        $message = ("{0}: Executing the REST query." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+        If ($PSBoundParameters['Verbose'] -or $VerbosePreference -eq 'Continue') { If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Verbose -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Verbose -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Verbose -Message $message } }
+
+        $stopLoop = $false
+        Do {
+            Try {
+                $response = Invoke-RestMethod -Uri $url -Method $httpVerb -Header $headers -ErrorAction Stop
+
+                $stopLoop = $True
+                $firstLoopDone = $True
+            } Catch {
+                If ($_.Exception.Message -match '429') {
+                    $message = ("{0}: Rate limit exceeded, retrying in 60 seconds." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $MyInvocation.MyCommand, $_.Exception.Message)
+                    If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Warning -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Warning -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Warning -Message $message }
+
+                    Start-Sleep -Seconds 60
+                } ElseIf ($_.ErrorDetails -match 'invalid filter') {
+                    $message = ("{0}: LogicMonitor returned `"invalid filter`". Please validate the value of the -Filter parameter and try again." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+                    If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Error -Message $message -BlockStdErr $BlockStdErr } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Error -Message $message -BlockStdErr $BlockStdErr } Else { Out-PsLogging -ScreenOnly -MessageType Error -Message $message -BlockStdErr $BlockStdErr }
+
+                    Return "Error"
+                } Else {
+                    $message = ("{0}: Unexpected error getting devices. To prevent errors, {1} will exit. If present, the following details were returned:`r`n
+                        Error message: {2}`r
+                        Error code: {3}`r
+                        Invoke-Request: {4}`r
+                        Headers: {5}`r
+                        Body: {6}" -f
+                        ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $MyInvocation.MyCommand, ($_ | ConvertFrom-Json -ErrorAction SilentlyContinue | Select-Object -ExpandProperty errorMessage),
+                        ($_ | ConvertFrom-Json -ErrorAction SilentlyContinue | Select-Object -ExpandProperty errorCode), $_.Exception.Message, ($headers | Out-String), ($data | Out-String)
+                    )
+                    If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Error -Message $message -BlockStdErr $BlockStdErr } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Error -Message $message -BlockStdErr $BlockStdErr } Else { Out-PsLogging -ScreenOnly -MessageType Error -Message $message -BlockStdErr $BlockStdErr }
+
+                    Return "Error"
+                }
+            }
+        }
+        While ($stopLoop -eq $false)
+
+        If ($firstLoopDone -and ($response.items.Count -gt 0)) {
+            $message = ("{0}: Found {1} more devices." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $response.items.Count)
+            If ($PSBoundParameters['Verbose'] -or $VerbosePreference -eq 'Continue') { If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Verbose -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Verbose -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Verbose -Message $message } }
+
+            $devices.AddRange([System.Collections.Generic.List[PSObject]]@($response.items))
+
+            If ($response.items.Count -eq 1) {
+                $stopLoop = $true
+            } Else {
+                $stopLoop = $false
+            }
+        } ElseIf ($firstLoopDone -and $response.id) {
+            $devices = $response
+
+            $stopLoop = $true
+        } Else {
+            $message = ("{0}: The `$response variable is empty." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"))
+            If ($PSBoundParameters['Verbose'] -or $VerbosePreference -eq 'Continue') { If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Verbose -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Verbose -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Verbose -Message $message } }
+
+            $stopLoop = $true
+        }
+
+        $message = ("{0}: There are {1} devices in `$devices." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $devices.id.Count)
+        If ($PSBoundParameters['Verbose'] -or $VerbosePreference -eq 'Continue') { If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Verbose -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Verbose -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Verbose -Message $message } }
+
+        If ($stopLoop -eq $false) {
+            # Increment offset, to grab the next batch of devices.
+            $message = ("{0}: Incrementing the search offset by {1}." -f ([datetime]::Now).ToString("yyyy-MM-dd`THH:mm:ss"), $BatchSize)
+            If ($PSBoundParameters['Verbose'] -or $VerbosePreference -eq 'Continue') { If ($EventLogSource -and (-NOT $LogPath)) { Out-PsLogging -EventLogSource $EventLogSource -MessageType Verbose -Message $message } ElseIf ($LogPath -and (-NOT $EventLogSource)) { Out-PsLogging -LogPath $LogPath -MessageType Verbose -Message $message } Else { Out-PsLogging -ScreenOnly -MessageType Verbose -Message $message } }
+
+            $offset += $BatchSize
+        }
+    }
+    Until (($stopLoop -eq $true) -or ($singleDeviceCheckDone))
+
+    $devices
+} #1.0.0.0
